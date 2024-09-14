46be3939e50a31681a3b06591e9fdda6c70f66eb
Marcelo <marceloromero2016ar@gmail.com>
2024-09-14 13:59:07 +0200

    fix: deploy

--- app/api/expenses/[id]/route.ts
+++ app/api/expenses/[id]/route.ts
@@ -1,4 +1,3 @@
-// pages/api/expenses/[id]/route.ts
 import { PrismaClient } from "@prisma/client";
 import type { NextRequest } from 'next/server';
 
@@ -7,17 +6,19 @@ const prisma = new PrismaClient();
 // Gestion de la méthode PUT pour mettre à jour une charge
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   const { id } = params;
+
   try {
     const { name, amount, category } = await req.json();
+
     if (!name || !amount || !category) {
       return new Response(JSON.stringify({ error: 'Missing required fields' }), { status: 400 });
     }
 
     const updatedExpense = await prisma.fixedExpense.update({
-      where: { id },
+      where: { id }, // ID en tant que string
       data: {
         name,
-        amount: parseFloat(amount),
+        amount: parseFloat(amount), // Assurez-vous que 'amount' est un nombre
         category,
       },
     });
@@ -27,16 +28,20 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
+    console.error('Error updating expense:', error);
     return new Response(JSON.stringify({ error: 'Error updating expense' }), { status: 500 });
+  } finally {
+    await prisma.$disconnect(); // Fermer l'instance Prisma
   }
 }
 
 // Gestion de la méthode DELETE pour supprimer une charge
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
   const { id } = params;
+
   try {
     await prisma.fixedExpense.delete({
-      where: { id },
+      where: { id }, // ID en tant que string
     });
 
     return new Response(JSON.stringify({ message: 'Expense deleted successfully' }), {
@@ -44,6 +49,9 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
+    console.error('Error deleting expense:', error);
     return new Response(JSON.stringify({ error: 'Error deleting expense' }), { status: 500 });
+  } finally {
+    await prisma.$disconnect(); // Fermer l'instance Prisma
   }
 }
